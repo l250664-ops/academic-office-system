bb2c3bc10dfbed2292678f18b45aaa84e5e52143
oop final project <l250664@lhr.nu.edu.pk>
2026-05-10 18:15:13 +0500

    Implement Student save and load functions in DatabaseManager.cpp

---
+++ src/DatabaseManager.cpp
@@ -0,0 +1,55 @@
+#include "../include/DatabaseManager.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+DatabaseManager::DatabaseManager() {
+    studentsFile = "students.txt";
+    teachersFile = "teachers.txt";
+    coursesFile = "courses.txt";
+    venuesFile = "venues.txt";
+    sectionsFile = "sections.txt";
+}
+
+// ---- STUDENT ----
+void DatabaseManager::saveStudent(Student* s) {
+    ofstream file(studentsFile, ios::app);
+    if (!file) { cout << "Error opening students file." << endl; return; }
+    file << s->getID() << " | " << s->getName() << " | "
+        << s->getStudentType() << " | " << s->calculateGPA() << endl;
+    file.close();
+}
+
+void DatabaseManager::loadStudents(Student* students[], int& count) {
+    ifstream file(studentsFile);
+    if (!file) { cout << "No students file found." << endl; return; }
+    string line;
+    count = 0;
+    while (getline(file, line) && count < 50) {
+        stringstream ss(line);
+        string id, name, type, gpaStr;
+        getline(ss, id, '|');
+        getline(ss, name, '|');
+        getline(ss, type, '|');
+        getline(ss, gpaStr);
+
+        auto trim = [](string s) {
+            while (!s.empty() && s[0] == ' ') s = s.substr(1);
+            while (!s.empty() && s.back() == ' ') s.pop_back();
+            return s;
+            };
+        id = trim(id); name = trim(name);
+        type = trim(type); gpaStr = trim(gpaStr);
+
+        float gpa = stof(gpaStr);
+
+        if (type == "Regular")
+            students[count++] = new RegularStudent(id, name, "", gpa);
+        else if (type == "Scholarship")
+            students[count++] = new ScholarshipStudent(id, name, "", gpa, 2.5);
+        else if (type == "Exchange")
+            students[count++] = new ExchangeStudent(id, name, "", gpa);
+    }
+    file.close();
+}
