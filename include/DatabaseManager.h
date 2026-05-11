eded3553a1309504e59ca00e9681f53e08503e74
oop final project <l250664@lhr.nu.edu.pk>
2026-05-11 11:56:56 +0500

    Add Scheduler header file

--- include/DatabaseManager.h
+++ include/DatabaseManager.h
@@ -1,1 +1,36 @@
 #pragma once
+#include "Student.h"
+#include "Teacher.h"
+#include "Course.h"
+#include "Venue.h"
+#include "Section.h"
+#include <string>
+#include <fstream>
+using namespace std;
+
+class DatabaseManager {
+private:
+    string studentsFile;
+    string teachersFile;
+    string coursesFile;
+    string venuesFile;
+    string sectionsFile;
+
+public:
+    DatabaseManager();
+
+    void saveStudent(Student* s);
+    void loadStudents(Student* students[], int& count);
+
+    void saveTeacher(Teacher* t);
+    void loadTeachers(Teacher* teachers[], int& count);
+
+    void saveCourse(Course* c);
+    void loadCourses(Course* courses[], int& count);
+
+    void saveVenue(Venue* v);
+    void loadVenues(Venue* venues[], int& count);
+
+    void saveSection(Section* s);
+    void loadSections(Section* sections[], int& count);
+};
